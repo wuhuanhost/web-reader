e6593aac2c701f1275c62ae329bd5c237b704bd8
wuhuanhost <post.thinking@gmail.com>
2016-09-05 18:54:09 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 require('./styles/style.css');
 import 'styles/print.css';
-import 'swiper/dist/js/swiper.js';
+import Swiper from 'swiper/dist/js/swiper.js';
 import 'swiper/dist/css/swiper.css';
 
-var mySwiper = new Swiper ('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: false,
     // 如果需要前进后退按钮
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
-	pagination : '.swiper-pagination',
-	paginationType : 'fraction'
-	
-})
-		
+    pagination: '.swiper-pagination',
+    paginationType: 'fraction'
+});
+
+
+
+
+
+
+
+
+
+
 require('./lib/css-regions-polyfill.js');
